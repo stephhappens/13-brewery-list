9d7bd27be31b7ffc23f29e033dee981b93b0951e
Derrick Bray <derrickbray@yahoo.com>
2016-10-24 16:39:57 -0500

    js works

--- app/views/app.js
+++ app/views/app.js
@@ -5,16 +5,25 @@ export default class ResultListView {
   }
 
   render() {
+    this.el.innerHTML = '';
+
     this.model.forEach((result) => {
       const row = document.createElement('div');
       row.classList.add('result');
 
       row.innerHTML = `
-      <img class="result__logo" src="">
-      <h2 class="result__name"></h2>
-      <p class="result__year"></p>`;
+      <div class="result__logo">
+        <img src="http://lorempixel.com/output/animals-q-c-640-480-8.jpg" class="result__logo-photo" alt="puppy">
+      </div>
+      <div class="result__content">
+        <h2 class="result__name"> I am a brewery name.</h2>
+        <p class="result__year"> I was established on </p>
+      </div>`;
 
-      row.querySelector('.result__logo').innerText = result.images.icon;
+      if (result.images) {
+        row.querySelector('.result__logo-photo').src = result.images.icon;
+      }
+
       row.querySelector('.result__name').innerText = result.name;
       row.querySelector('.result__year').innerText = result.established;
 
